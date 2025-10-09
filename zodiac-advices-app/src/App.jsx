9c3132f2b1072bdcab7513d0a97d81d17afc3b89
Арайлым Мырзаханкызы <a_myrzakhankyzy@kbtu.kz>
2025-10-09 20:16:34 +0500

    Add zodiac-advices-app project

---
+++ zodiac-advices-app/src/App.jsx
@@ -0,0 +1,12 @@
+import ZodiacList from './components/ZodiacList';
+import './App.css'; 
+
+function App() {
+  return (
+    <div className="App">
+      <ZodiacList />
+    </div>
+  );
+}
+
+export default App;
